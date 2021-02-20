a215e07397be1b665789e6ad2dadb7f3b464e4d5
Yannis Guéguen <33959654+yanniscode@users.noreply.github.com>
2021-02-20 22:55:37 +0100

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,7 +15,7 @@ const Op = Sequelize.Op;
 
 const sequelize = new Sequelize("dataviz_fish_uk", "root", "test", {
 
-    host: "51.210.254.67",
+    host: "localhost",
     dialect: "mysql",
 
     pool: {
